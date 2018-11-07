c097b8db7ea1b2f8131d229188e20ec8d0ecdd83
Max Desiatov <max@desiatov.com>
2018-11-07 09:47:55 +0000

    Updated dependencies and error handling

--- Package.swift
+++ Package.swift
@@ -9,13 +9,15 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/maxdesiatov/CoreXLSX.git",
                 .branch("master")),
+        .package(url: "https://github.com/yaslab/CSV.swift.git",
+                 .upToNextMinor(from: "2.2.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "toptal2freeagent",
-            dependencies: ["CoreXLSX"]),
+            dependencies: ["CoreXLSX", "CSV"]),
         .testTarget(
             name: "toptal2freeagentTests",
             dependencies: ["toptal2freeagent"]),
--- Sources/toptal2freeagent/main.swift
+++ Sources/toptal2freeagent/main.swift
@@ -1,4 +1,5 @@
 import CoreXLSX
+import CSV
 
 guard CommandLine.arguments.count > 1,
 let filepath = CommandLine.arguments.last,
@@ -6,7 +7,15 @@ let file = XLSXFile(filepath: filepath) else {
   fatalError("please pass path to XLSX from Toptal Payments to parse")
 }
 
-
-for ws in try file.parseWorksheetPaths() {
-  print(try file.parseWorksheet(at: ws))
+do {
+  for path in try file.parseWorksheetPaths() {
+    let ws = try file.parseWorksheet(at: path)
+    for row in ws.sheetData.rows {
+      for c in row.cells {
+        print(c)
+      }
+    }
+  }
+} catch {
+  print(error)
 }
